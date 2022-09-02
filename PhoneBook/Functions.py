afb90efc621753b12fc4661cf9bde26e4bebe901
sadegh.hokmabadi <sadegh.hokmabadi@gmail.com>
2022-09-02 22:10:39 +0430

    complet functions - add csv file - save and read from csv file

--- PhoneBook/Functions.py
+++ PhoneBook/Functions.py
@@ -1,4 +1,3 @@
-
 def displaymenu():
     menu = """
 ==================================================
@@ -7,6 +6,8 @@ def displaymenu():
 =>      2: List
 =>      3: search by name
 =>      4: search by mobile
+=>      5: edit
+=>      6: delete
 =>      0: exit
 
 ==================================================
@@ -15,37 +16,70 @@ def displaymenu():
     q = int(input("Choise a number: "))
     return q
 
-
-#def readfile():
-#    contacts=[]
-#    f = open("Contacts.txt","r")
-#    try:
-#        for line in f:
-#            contact= line[:-1].split(',')
-#            contacts.append(contact)
-#    finally:
-#        f.close()
-#        return contacts
-
 def readfile():
-    contacts=[]
-    with open("Contacts.txt","r") as f:
+    contacts = []
+    with open("Contacts.csv","r") as f:
         for line in f:
-            contact= line[:-1].split(',')
+            contact = line[:-1].split(',')
             contacts.append(contact)
     return contacts
 
-
 def savefile(contacts):
-    f=open("Contacts.txt","w")
-    try:
+   with open("Contacts.csv","w") as f:
         for contact in contacts:
-            f.write(contact[0]+',')
-            f.write(contact[1]+'\n')
-    finally:
-        f.close()
+            f.write(contact[0] + ',')
+            f.write(contact[1] + '\n')   
 
-    
-    
+def add(contacts):
+    print("================ Enter a Contact =================")
+    name = input("Enter Name: ")
+    mobile = input("Enter mobile: ")
+    #contact=[name,mobile]
+    contacts.append([name,mobile])
+    savefile(contacts)
+    return contacts
 
+def showList(contacts):
+     print("=============== list of Contacts =================")
+     print("         ID----------",contacts[0][0],"----------",contacts[0][1])
+     for i in range(1,len(contacts)):
+        print("        ",i,"----------",contacts[i][0],"----------",contacts[i][1])
+     print("==================================================")
 
+def search_by_name(contacts):
+    print("=============== Search by Name ===================")
+    name = input("Enter Name for search: ")
+    for i in range(len(contacts)):
+        if contacts[i][0] == name:
+            print("mobile ",contacts[i][0]," is ",contacts[i][1])
+            break    
+
+def search_by_mobile(contacts):
+    print("=============== Search by Mobile =================")
+    mobile = input("Enter Mobile for search: ")
+    for i in range(len(contacts)):
+        if contacts[i][1] == mobile:
+            print("mobile ",contacts[i][0]," is ",contacts[i][1])
+            break  
+
+def edit(contacts):
+    showList(contacts)
+    id = int(input("Choose an ID from the list above: "))
+    name = input("Enter New Name (if not change press Enter): ")
+    mobile = input("Enter New Mobile (if not change press Enter): ")
+    if name != '':
+        contacts[id][0] = name
+    if mobile != '':
+        contacts[id][1] = mobile
+    savefile(contacts)
+    print("Edit successful")
+   
+def delete(contacts):
+    df=pd.read_csv('Contacts.csv')
+    showList(contacts)
+    id = int(input("Choose an ID from the list above: "))
+    r = input("Are you sure to delete " + contacts[id][0]+" (y OR n)? ")
+    if r == 'y':
+        del contacts[id]
+    savefile(contacts)
+    print("Delete successful")
--- PhoneBook/PhoneBook.py
+++ PhoneBook/PhoneBook.py
@@ -1,46 +1,27 @@
 from Functions import *
 
-contacts = []
-
-q = displaymenu()
 contacts = readfile()
 
-while q != 0 :
+while True :
+    q = displaymenu()
+    #exit
+    if q == 0:
+        savefile(contacts)
+        break
     # Add Contact
-    if q == 1:
-        print("================ Enter a Contact =================")
-        name = input("Enter Name: ")
-        mobile = input("Enter mobile: ")
-        #contact=[name,mobile]
-        contacts.append([name,mobile])
+    elif q == 1:
+        contacts = add(contacts)
     # List Contacts
     elif q == 2:
-        print("""=============== list of Contacts =================
-
-        id ---------- name --------- mobile """)
-        for i in range(len(contacts)):
-            print("        ",i,"----------",contacts[i][0],"----------",contacts[i][1])
-        print("""
-==================================================""")
+       showList(contacts)
     # Search by name
     elif q == 3:
-        print("=============== Search by Name ===================")
-        name = input("Enter Name for search: ")
-        for i in range(len(contacts)):
-            if contacts[i][0] == name:
-                print("mobile ",contacts[i][0]," is ",contacts[i][1])
-                break    
+        search_by_name(contacts)
     # Search by mobile
     elif q == 4:
-        print("=============== Search by Mobile =================")
-        mobile = int(input("Enter Mobile for search: "))
-        for i in range(len(contacts)):
-            if contacts[i][1] == mobile:
-                print("mobile ",contacts[i][0]," is ",contacts[i][1])
-                break  
+       search_by_mobile(contacts)
+    elif q == 5:
+        edit(contacts)
+    elif q==6:
+        delete(contacts)
 
-    q = displaymenu()
-
-if q==0:
-    savefile(contacts)
-        
